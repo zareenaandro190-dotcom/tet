c74188c110b652cbe47504aae65f615f041dd6f9
zareena andro <zareenaandro190@gmail.com>
2025-10-21 10:51:11 +0000

    Try fixing this error: `Runtime Error: Error: Connection closed.. Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-      maxDuration: 120,
+    experimental: {
+        bodySizeLimit: '6mb',
     },
-  },
 };
 
 export default nextConfig;
